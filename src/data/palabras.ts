b0d32c7627382e2dc93b2d332835034829e06212
Carlos Andrés Arévalo Torres <carlosandresat@hotmail.com>
2024-07-18 17:18:37 -0500

    Update palabras array

--- src/data/palabras.ts
+++ src/data/palabras.ts
@@ -1,8 +1,5 @@
 export const palabras = [
-    "gato", "perro", "pez", "pájaro", "elefante", "jirafa", "león", "tigre",
-    "cocodrilo", "delfín", "ballena", "rinoceronte", "hipopótamo", "murciélago",
-    "panda", "orangután", "canguro", "koala", "búho", "zorro", "mono", "oso",
-    "lobo", "ardilla", "mapache", "serpiente", "tortuga", "iguana", "camaleón",
-    "pingüino", "foca", "lobomarino", "nutria", "puma", "pantera", "camello",
-    "dromedario", "ciervo", "alce", "antílope"
-  ];
+  "arbol", "camisa", "cerdo", "conejo", "pato", "avión", "casa", "pez", "gato",
+  "gallo", "barco", "cohete", "jinete", "tetera", "cisne", "espada",
+  "pirámide", "grulla", "martillo", "serpiente"
+];
