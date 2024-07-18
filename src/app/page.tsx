40e0648f33433ba90e3ffaf00a829802d43adb52
Carlos Andrés Arévalo Torres <carlosandresat@hotmail.com>
2024-07-18 18:05:29 -0500

    Move ModeToggle to left and moved to navbar for mobile devices

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,10 @@ export default function Home() {
   return (
     <>
       <header className="px-4 lg:px-6 h-16 flex items-center border-b fixed top-0 w-full bg-background z-10">
+        <div className="md:hidden">
+          <ModeToggle></ModeToggle>
+        </div>
+
         <nav className="ml-auto flex gap-4 sm:gap-6">
           <Link
             className="text-sm font-medium hover:underline underline-offset-4"
@@ -17,9 +21,10 @@ export default function Home() {
       </header>
 
       <main className="flex flex-col items-center p-8 w-full min-h-[calc(100vh-65px)] pt-24">
-        <div className="self-end">
+        <div className="self-start hidden md:block">
           <ModeToggle></ModeToggle>
         </div>
+
         <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl mt-6 text-center">
           Juego de Ordenar Palabras
         </h1>
